aefb68798a3ac8592b9cf0dfb105f25d157d83ed
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-04-08 20:44:49 -0300

    Criação Crud User e autenticação jwt

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,46 @@ class UserController {
 
     return res.json(user);
   }
+  async index(req, res) {
+    const user = await User.find();
+    return res.json(user);
+  }
+
+  async show(req, res) {
+    const { id } = req.params.id;
+
+    if (!(await User.findOne({ id }))) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const user = await User.findOne({ id });
+
+    return res.json(user);
+  }
+  async destroy(req, res) {
+    const { id } = req.params.id;
+
+    if (!(await User.findOne({ id }))) {
+      return res.status(400).json({ error: "Id invalid" });
+    }
+
+    await User.findOneAndRemove({ id });
+
+    return res.json({ menssage: "Sucess" });
+  }
+  async update(req, res) {
+    const { id } = req.params.id;
+
+    if (!(await User.findOne({ id }))) {
+      return res.status(400).json({ error: "Id invalid" });
+    }
+
+    const user = await User.findByIdAndUpdate({ id }, req.body, {
+      new: true
+    });
+
+    return res.json(user);
+  }
 }
 
 module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,14 @@ const express = require("express");
 const routes = express.Router();
 
 const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
 
+routes.get("/index", UserController.index);
+routes.get("/show/:id", UserController.show);
+routes.delete("/delete/:id", UserController.destroy);
+routes.put("/update/:id", UserController.update);
 routes.post("/users", UserController.store);
+
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
