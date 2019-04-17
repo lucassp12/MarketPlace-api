64eaefb4ae0d561db8721516130f6044a04fbe6b
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-04-16 21:30:22 -0300

    Crud de Ad

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,23 @@ const authMiddleware = require("./app/middlewares/auth");
 
 const controllers = require("./app/controllers");
 
+//User
 routes.get("/index", controllers.UserController.index);
 routes.get("/show/:id", controllers.UserController.show);
 routes.delete("/delete/:id", controllers.UserController.destroy);
 routes.put("/update/:id", controllers.UserController.update);
 routes.post("/users", controllers.UserController.store);
 
+//Session
 routes.post("/sessions", controllers.SessionController.store);
 
-routes.get("/test", authMiddleware, (req, res) => res.json({ ok: true }));
+routes.use(authMiddleware);
+
+//Ads
+routes.get("/ads", controllers.AdController.index);
+routes.get("/ads/:id", controllers.AdController.show);
+routes.post("/ads", controllers.AdController.store);
+routes.put("/ads/:id", controllers.AdController.update);
+routes.delete("/ads/:id", controllers.AdController.destroy);
 
 module.exports = routes;
