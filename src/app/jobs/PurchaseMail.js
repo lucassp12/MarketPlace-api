68357db5cf0fbddad5ab9a8bd992a79dfa89302b
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-04-23 20:18:55 -0300

    Configurando fila

---
+++ src/app/jobs/PurchaseMail.js
@@ -0,0 +1,20 @@
+const Mail = require("../services/Mail");
+
+class PurchaseMail {
+  get key() {
+    return "PurchaseMail";
+  }
+  async handle(job, done) {
+    const { ad, user, content } = job.data;
+    await Mail.sendMail({
+      from: '"Lucas Santos" <lucaspoter2@gmail.com>',
+      to: ad.author.email,
+      subject: `Solitação de compra: ${ad.title}`,
+      template: "purchase",
+      context: { user, content, ad }
+    });
+
+    return done();
+  }
+}
+module.exports = new PurchaseMail();
